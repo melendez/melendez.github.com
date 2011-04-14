fd8f56ef44e95f4f75a1fed4d47eb41bf022672d
Miguel Mota <admin@mrgproperties.com>
2011-04-14 00:41:14 -0700

    ...

---
+++ _site/scripts/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	
+	// check pathname and add selected class to nav link
+	var pathname = window.location.pathname;
+	$('ul.nav li a').each(function(){
+		if($(this).attr('href') == pathname){
+			$(this).addClass('selected');
+		}
+	});	
+	
+	$('ul.nav li').hover(function(){
+		$(this).find('ul.subnav').slideDown('fast').show();
+		},
+		function(){
+			$(this).find('ul.subnav').slideUp('fast');
+		}
+	);
+
+});
+
+$(window).load(function(){
+	
+	// initialize nivo slider
+	$('#slider').nivoSlider();
+	
+});
