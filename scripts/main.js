45515cb9672d47e4f220ae36b2f4272d9ae9b077
Miguel Mota <miguelmota2@gmail.com>
2011-04-18 18:05:18 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -21,9 +21,6 @@ $(document).ready(function(){
 		$('div#feed ul li').css('visibility', 'visible');
 	});
 	
-	//load google feeds
-	google.load('feeds','1');
-	
 });
 
 $(window).load(function(){
@@ -34,33 +31,5 @@ $(window).load(function(){
 	
 });
 
-
-var feedcontainer = document.getElementById('feed');
-var feedurl = 'http://www.houselogic.com/news/rss/';
-var feedlimit = 10;
-var rssoutput = '<strong>Latest HouseLogic News:</strong><ul>';
-
-function rssfeedsetup() {
-	var feedpointer = new google.feeds.Feed(feedurl);
-	feedpointer.setNumEntries(feedlimit);
-	feedpointer.load(displayfeed);
-}
-
-function displayfeed(result){
-	if(!result.error){
-		var thefeeds = result.feed.entries;
-		for(var i=0; i < thefeeds.length; i++){
-			rssoutput += "<li>&#187; <a href='"+thefeeds[i].link+"'>"+thefeeds[i].title+"</a></li>";
-		}
-		rssoutput += "</ul><a class='more' href='javascript:void(0);'>more &#187;</a>";
-		feedcontainer.innerHTML = rssoutput;
-		//$('div#feed ul li:gt(5)').css('visibility', 'hidden');
-	}
-	else {
-		document.getElementById('feed').innerHTML = '[Error fetching feeds]';
-	}
-}
-
-window.onload = function(){
-	rssfeedsetup();
-}
+//load google feeds
+google.load('feeds','1');
