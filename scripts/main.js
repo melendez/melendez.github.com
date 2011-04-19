90c1a644be2d3c0121e2fe8d25bba91d0cc373c7
Miguel Mota <miguelmota2@gmail.com>
2011-04-18 18:02:49 -0700

    ...

--- scripts/main.js
+++ scripts/main.js
@@ -27,9 +27,41 @@ $(window).load(function(){
 	
 	// initialize nivo slider
 	$('#slider').nivoSlider();
-
+	
+	
+	rssfeedsetup();
 	
 });
 
 //load google feeds
 google.load('feeds','1');
+
+var feedcontainer = document.getElementById('feed');
+var feedurl = 'http://www.houselogic.com/news/rss/';
+var feedlimit = 10;
+var rssoutput = '<strong>Latest HouseLogic News:</strong><ul>';
+
+function rssfeedsetup() {
+	var feedpointer = new google.feeds.Feed(feedurl);
+	feedpointer.setNumEntries(feedlimit);
+	feedpointer.load(displayfeed);
+}
+
+function displayfeed(result){
+	if(!result.error){
+		var thefeeds = result.feed.entries;
+		for(var i=0; i < thefeeds.length; i++){
+			rssoutput += "<li>&#187; <a href='"+thefeeds[i].link+"'>"+thefeeds[i].title+"</a></li>";
+		}
+		rssoutput += "</ul><a class='more' href='javascript:void(0);'>more &#187;</a>";
+		feedcontainer.innerHTML = rssoutput;
+		//$('div#feed ul li:gt(5)').css('visibility', 'hidden');
+	}
+	else {
+		document.getElementById('feed').innerHTML = '[Error fetching feeds]';
+	}
+}
+
+window.onload = function(){
+	
+}
