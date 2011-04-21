170ae5d7f32ee3cb45787eb888f23c59062fa305
Miguel Mota <miguelmota2@gmail.com>
2011-04-21 13:21:13 -0700

    ...

--- _site/scripts/main.js
+++ _site/scripts/main.js
@@ -2,13 +2,13 @@ $(document).ready(function(){
 	
 	// check pathname and add selected class to nav link
 	var pathname = window.location.pathname;
-	$('ul.nav li a').each(function(){
+	$('nav ul#nav li a').each(function(){
 		if($(this).attr('href') == pathname){
 			$(this).addClass('selected');
 		}
 	});	
 	
-	$('ul.nav li').hover(function(){
+	$('ul#nav li').hover(function(){
 		$(this).find('ul.subnav').slideDown('fast').show();
 		},
 		function(){
@@ -21,6 +21,11 @@ $(document).ready(function(){
 $(window).load(function(){
 	
 	// initialize nivo slider
-	$('#slider').nivoSlider();
+	$('div#slider').nivoSlider({
+		pauseTime: 5000
+	});
 	
 });
+
+//load google feeds
+google.load('feeds','1');
